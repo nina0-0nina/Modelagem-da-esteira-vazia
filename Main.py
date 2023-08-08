32a9ae9d1328e4f41914b3f77f1265808bf1ffbe
Marina Calheira de Oliveira <calheiraoliveira@gmail.com>
2023-08-08 14:32:15 -0300

    .

--- Main.py
+++ Main.py
@@ -1,1 +1,40 @@
 # Criação das variáveis
+
+# Frequência do inversor
+Frequencia_3000 = 3000
+Frequencia_3200 = 3200
+Frequencia_3400 = 3400
+Frequencia_3600 = 3600
+Frequencia_3800 = 3800
+Frequencia_4000 = 4000
+Frequencia_4200 = 4200
+Frequencia_4400 = 4400
+Frequencia_4600 = 4600
+Frequencia_4800 = 4800
+Frequencia_5000 = 5000
+Frequencia_5200 = 5200
+Frequencia_5400 = 5400
+Frequencia_5600 = 5600
+Frequencia_5800 = 5800
+Frequencia_6000 = 6000
+
+# Tempo em segundos
+
+Frequencia_3000_tempo = 177
+Frequencia_3200_tempo = 167
+Frequencia_3400_tempo = 159
+Frequencia_3600_tempo = 150.5
+Frequencia_3800_tempo = 143.5
+Frequencia_4000_tempo = 137
+Frequencia_4200_tempo = 131
+Frequencia_4400_tempo = 126
+Frequencia_4600_tempo = 122
+Frequencia_4800_tempo = 117.5
+Frequencia_5000_tempo = 114
+Frequencia_5200_tempo = 110
+Frequencia_5400_tempo = 107
+Frequencia_5600_tempo = 104.5
+Frequencia_5800_tempo = 103
+Frequencia_6000_tempo = 100
+
+
